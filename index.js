eb591b189f8ac5dc9f1f322f7cfd7af06182a304
Maqsood Ahmed <maqsoodnasirani421@gmail.com>
2025-04-11 22:51:27 +0500

    rendered home page

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ const PORT = 8000;
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
 
+
+app.get('/', (req, res)=> {
+    res.render('home');
+})
+
 app.listen(PORT, ()=> console.log(`Server Started at PORT:${PORT}`)); 
