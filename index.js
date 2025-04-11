e99dbafe20cebe9b265a003e489bada19cf1ded1
Maqsood Ahmed <maqsoodnasirani421@gmail.com>
2025-04-11 22:27:29 +0500

    Initializing git

---
+++ index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+const PORT = 8000;
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"));
+
+app.listen(PORT, ()=> console.log(`Server Started at PORT:${PORT}`)); 
